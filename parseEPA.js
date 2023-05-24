4f9ee77f4ec8d5c3a39a05f4d72b266f8fdf427e
Sergismundo <sergioespe4@gmail.com>
2023-05-24 20:01:16 +0200

    filter incorrect requests

--- parseEPA.js
+++ parseEPA.js
@@ -5,9 +5,10 @@ export async function parseEPA(content) {
   console.log(rows.length);
 
   rows.forEach((row) => {
-    let partes = row.split(" ");
-    let host = partes[0];
-    let dateParts = partes[1].substring(1, partes[1].length - 1).split(":");
+    let parts = row.split(" ");
+    if (parts.length !== 7) return;
+    let host = parts[0];
+    let dateParts = parts[1].substring(1, parts[1].length - 1).split(":");
     let dateTime = {
       day: dateParts[0],
       hour: dateParts[1],
@@ -15,16 +16,16 @@ export async function parseEPA(content) {
       second: dateParts[3],
     };
     let request = {
-      method: partes[2].substring(1, partes[2].length),
-      url: partes[3].substring(0, partes[3].length),
-      protocol: partes[4].substring(0, 4),
-      protocol_version: partes[4].substring(5, 8),
+      method: parts[2].substring(1, parts[2].length),
+      url: parts[3].substring(0, parts[3].length),
+      protocol: parts[4].substring(0, 4),
+      protocol_version: parts[4].substring(5, 8),
     };
-    let response_code = partes[5];
+    let response_code = parts[5];
     let failCode = response_code !== "200";
     let document_size = failCode
       ? "0"
-      : partes[6].substring(0, partes[6].length - 1);
+      : parts[6].substring(0, parts[6].length - 1);
 
     let epa_object = {
       host,
